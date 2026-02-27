0a6b6bc9b1c151ff2404d6f9857b3c2626d1929f
Daylin Morgan <me@dayl.in>
2026-02-27 14:41:49 -0500

    check pos again

--- tests/tofficial.nim
+++ tests/tofficial.nim
@@ -27,11 +27,7 @@ template checkError(path: string) =
   except UsuParserError as e:
     var message = readFile(path / "out.msg")
     message.stripLineEnd()
-    # \r\n handling makes the pos unstable so ignore it on windows
-    when defined(windows):
-      check e.msg.split("pos")[0] == message.split("pos")[0]
-    else:
-      check e.msg == message
+    check e.msg == message
 
 suite "official failing":
   for path in failing:
