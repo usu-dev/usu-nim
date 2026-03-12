41b8822f2e389d1bf54bb48d33a3771bcd617896
Daylin Morgan <me@dayl.in>
2026-03-12 13:56:28 -0400

    json -> usu {node}

--- src/usu/json.nim
+++ src/usu/json.nim
@@ -15,4 +15,25 @@ proc `%`*(u: UsuNode): JsonNode =
   of UsuNull:
     result = newJNull()
 
+proc toUsu*(node: JsonNode): UsuNode =
+  case node.kind
+  of JString:
+    result = newUsuValue(node.str)
+  of JInt:
+    result = newUsuValue($node.num)
+  of JFloat:
+    result = newUsuValue($node.fnum)
+  of Jbool:
+    result = newUsuValue($node.bval)
+  of JNull:
+    result = newUsuNull()
+  of JObject:
+    result = UsuNode(kind: UsuMap)
+    for k, v in node.fields.pairs:
+      result.fields[k] = v.toUsu()
+  of JArray:
+    result = UsuNode(kind: UsuArray)
+    for v in node.elems:
+      result.elems.add v.toUsu()
+
 export json
